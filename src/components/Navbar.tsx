a55eb06228b9fd1b92710b6d721a1581c8ef4240
boodyf2 <abdalrahman.vim@gmail.com>
2024-10-14 11:41:50 +0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,18 @@
-const Navbar = () => {};
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Navbar = () => {
+    return (
+        <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex items-center justify-between h-14">
+                    <Link href="/" className="flex z-40 font-semibold">
+                        case<span className="text-green-600">cobra</span>
+                    </Link>
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    );
+};
 
 export default Navbar;
